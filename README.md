e442682ce35116ac08dd852dbd5359c5ed5f95b2
Matt Ewell <matt.ewell@gmail.com>
2021-03-20 15:23:04 -0600

    Add more tests

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # React Game of Life
 
-I originally wrote this four years ago or so as I was learning React and Redux. It has been touched up in 3/2021 to reflect more modern JS syntax. Redux was and is still overkill, and was used to practice using the library. It may be yanked in a future revision, and we may also see the class syntax disappear. Currently, I have removed the use of create-react-app, instead using a basic from scratch configuration.
+This is a React implementation of the zero player game, [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life).
+
+I originally wrote this in 2017 I was learning React and Redux. I'm revisiting as of Spring of 2021 as I try to pick up my learning where I left it.
+
+Redux was and is still overkill, and was used to practice using the library. It may be yanked in a future revision, and we may also see the class syntax disappear. Currently, I have removed the use of create-react-app, instead using a basic from scratch configuration. I've also modernized some of the JS.
 
 There is still cleanup to be done, and an infinite amount of improvement as I learn and practice more. But it's something.
 
--- test/utility/advanceBoard.test.js
+++ test/utility/advanceBoard.test.js
@@ -14,3 +14,60 @@ test("a blank board becomes a blank board", () => {
   ]
   expect(nextBoard).toEqual(expected)
 })
+
+test("a full 2x2 board becomes a full board", () => {
+  const board = [
+    [true, true],
+    [true, true],
+  ]
+  const nextBoard = advanceBoard(board)
+
+  const expected = [
+    [true, true],
+    [true, true],
+  ]
+  expect(nextBoard).toEqual(expected)
+})
+
+test("a dead corner cell comes back to life", () => {
+  const board = [
+    [true, true],
+    [false, true],
+  ]
+  const nextBoard = advanceBoard(board)
+
+  const expected = [
+    [true, true],
+    [true, true],
+  ]
+  expect(nextBoard).toEqual(expected)
+})
+
+test("cross-corner alive and dead die", () => {
+  const board = [
+    [true, false],
+    [false, true],
+  ]
+  const nextBoard = advanceBoard(board)
+
+  const expected = [
+    [false, false],
+    [false, false],
+  ]
+  expect(nextBoard).toEqual(expected)
+})
+
+test("3-in-a-row alternating pattern", () => {
+  const threeVertical = [
+    [false, true, false],
+    [false, true, false],
+    [false, true, false],
+  ]
+  const threeHorizontal = [
+    [false, false, false],
+    [true, true, true],
+    [false, false, false],
+  ]
+  expect(advanceBoard(threeVertical)).toEqual(threeHorizontal)
+  expect(advanceBoard(threeHorizontal)).toEqual(threeVertical)
+})
