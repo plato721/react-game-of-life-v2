329b5f9456e230fb18cbfe003eb282d38554f9a2
Matt Ewell <matt.ewell@gmail.com>
2021-03-20 17:42:31 -0600

    More arrow functions

--- actions/play_pause.js
+++ actions/play_pause.js
@@ -1,7 +1,12 @@
-export default function (intervalId = null) {
+// If we get an intervalId passed in, we've pressed play, and store the id
+//   to later destroy the setInterval that keeps the game moving if pause
+//   is pressed.
+const playPause = (intervalId = null) => {
   const action = { type: "PLAY_PAUSE" }
   if (intervalId) {
     action["intervalId"] = intervalId
   }
   return action
 }
+
+export default playPause
